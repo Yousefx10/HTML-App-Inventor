6b20f4295458a88d939a5ad22af0df06d5bdfed3
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-17 17:21:41 +0300

    Main Func to handle to adding process for kits

---
+++ project/project.js
@@ -0,0 +1,22 @@
+//Basic array list for active added elements to the project time_line and project_live
+let  active_kit =[];
+
+
+
+//[FUNCTIONS] Area
+function addKIT(kit_type)
+{
+    switch(kit_type){
+        case 1:
+            console.log('you have choose to add a label');
+            break;
+
+        case 2:
+            break;
+    }
+
+}
+
+
+
+
