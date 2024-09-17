f09fd128248588b782a98ce18597b8629a8de379
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-17 17:25:45 +0300

    Adding a label seems to be working fine

--- project/project.js
+++ project/project.js
@@ -1,14 +1,23 @@
 //Basic array list for active added elements to the project time_line and project_live
 let  active_kit =[];
 
-
+let project_timeline = document.getElementById('project_timeline');
 
 //[FUNCTIONS] Area
 function addKIT(kit_type)
 {
+    //kit type instructions :
+    //1 : label
+    //2 : text
     switch(kit_type){
         case 1:
-            console.log('you have choose to add a label');
+            active_kit.push(1);
+            console.log(active_kit);
+
+            let newParagraph = document.createElement('p');
+            newParagraph.textContent = "You've Added a label";
+            project_timeline.appendChild(newParagraph);
+
             break;
 
         case 2:
