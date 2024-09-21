72df7af92503bbc8898aa95486553a811ada3279
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-21 18:29:00 +0300

    adding important comments

--- project/project.js
+++ project/project.js
@@ -1,21 +1,27 @@
 
 
 
+
 //[FUNCTIONS] Area
+//this function runs when a new kit got born.
 function addKIT(kit_type)
 {
-    //Main element that will have the content for the new added element.
+    //[START] Main element that will have the content for the new added element.
     let newParagraph = document.createElement('p');
         newParagraph.classList.add('project_timeline_kit');
         newParagraph.id='test' + kit_type;
+    //[END] Main element that will have the content for the new added element.
 
-//this i can pass parameters without EXECUTE the function FROM FIRST TIME
+
+//this i can pass parameters without EXECUTE the function FROM FIRST TIME.
 newParagraph.onclick = () => timeline_properties('param1',newParagraph.innerHTML);
 
 
 
-
+//increasing the id counter.
     COUNT_KIT_PROGRESS++;
+
+    //Naming the kit before adding it to the timeline box.
     switch(kit_type){
 
         case 1:
@@ -28,7 +34,10 @@ newParagraph.onclick = () => timeline_properties('param1',newParagraph.innerHTML
             newParagraph.textContent = "You've Added a Text";
             break;
     }
+
+    //showing the current ARRAY that contains the kit's
     console.log(active_kit);
+    //finally, adding the kit to the timeline box.
     project_timeline.appendChild(newParagraph);
 
 }
@@ -49,6 +58,8 @@ function NameKit(kit_num)
     }
 }
 
+
+//this function manages timeline properties, and it occur everytime original kit get pressed
 function timeline_properties(current_kit,current_details)
 {
     properties_value.value =current_details;
