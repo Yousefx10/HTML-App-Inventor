24b4808b7e03eecadaa3e0df2fd25c3162cbbc0d
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-26 21:29:02 +0300

    Adding static events instead of dynamic ones because they should be structured when the page is built

--- project/workspace.js
+++ project/workspace.js
@@ -21,10 +21,10 @@ function Func_KIT(kit_name,kitID,KITtype)
 //This Function is used to Show The Suitable event's for the selected kit
 function show_Event_KIT(kitID,KITtype)
 {
-    Event_KIT();
     switch (KITtype)
         {
             case 3://Button
+
                 break;
             case 5://Timer
                 break;
@@ -32,19 +32,7 @@ function show_Event_KIT(kitID,KITtype)
 }
 
 //This Function Create ALL THE EVENT'S THERE
-function Event_KIT(kitID)
+function Event_KIT()
 {
-    const e_CLICK = document.createElement("p");
-    const e_LongPress = document.createElement("p");
-    const e_TimerDing = document.createElement("p");
 
-
-    e_CLICK.textContent = "On Click[]";
-    e_LongPress.textContent = "On LongPress[]";
-    e_TimerDing.textContent = "When Trick[]";
-
-
-    event_space.appendChild(e_CLICK);
-    event_space.appendChild(e_LongPress);
-    event_space.appendChild(e_TimerDing);
 }
