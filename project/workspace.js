0c3e4dee5ff2d684e1c6c19a9f081cd19f5ca3e9
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-26 20:30:41 +0300

    Clickable Functional Kit's

--- project/workspace.js
+++ project/workspace.js
@@ -3,10 +3,23 @@
 
 let kit_space = document.getElementById('kit_space_container');
 
+//This Function is used to add the functional kit's to appear on screen
 function Func_KIT(kit_name,kitID,KITtype)
 {
-    const neWelement = document.createElement("p");
-    neWelement.id="code" + kitID;
-    neWelement.textContent = kit_name;
-    kit_space.appendChild(neWelement);
+    const NewFuncKIT = document.createElement("p");
+    NewFuncKIT.id="code" + kitID;
+    NewFuncKIT.textContent = kit_name;
+
+
+    NewFuncKIT.onclick = () => Event_KIT(kitID,KITtype);
+
+    kit_space.appendChild(NewFuncKIT);
+}
+
+
+//This Function is used to Show The Suitable event's for the selected kit
+function Event_KIT(kitID,KITtype)
+{
+    console.log("Event_KIT",kitID);
+    console.log("Kit_Type",KITtype);
 }
