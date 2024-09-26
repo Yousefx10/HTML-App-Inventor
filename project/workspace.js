ac27031226e06c7adec78db1bebb7185a2d50261
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-26 21:14:14 +0300

    Adding Event's list to the event space

--- project/workspace.js
+++ project/workspace.js
@@ -2,6 +2,7 @@
 //then will be moved later to "variables.js"
 
 let kit_space = document.getElementById('kit_space_container');
+let event_space = document.getElementById('event_space_container');
 
 //This Function is used to add the functional kit's to appear on screen
 function Func_KIT(kit_name,kitID,KITtype)
@@ -11,15 +12,39 @@ function Func_KIT(kit_name,kitID,KITtype)
     NewFuncKIT.textContent = kit_name;
 
 
-    NewFuncKIT.onclick = () => Event_KIT(kitID,KITtype);
+    NewFuncKIT.onclick = () => show_Event_KIT(kitID,KITtype);
 
     kit_space.appendChild(NewFuncKIT);
 }
 
 
 //This Function is used to Show The Suitable event's for the selected kit
-function Event_KIT(kitID,KITtype)
+function show_Event_KIT(kitID,KITtype)
 {
-    console.log("Event_KIT",kitID);
-    console.log("Kit_Type",KITtype);
+    Event_KIT();
+    switch (KITtype)
+        {
+            case 3://Button
+                break;
+            case 5://Timer
+                break;
+        }
+}
+
+//This Function Create ALL THE EVENT'S THERE
+function Event_KIT(kitID)
+{
+    const e_CLICK = document.createElement("p");
+    const e_LongPress = document.createElement("p");
+    const e_TimerDing = document.createElement("p");
+
+
+    e_CLICK.textContent = "On Click[]";
+    e_LongPress.textContent = "On LongPress[]";
+    e_TimerDing.textContent = "When Trick[]";
+
+
+    event_space.appendChild(e_CLICK);
+    event_space.appendChild(e_LongPress);
+    event_space.appendChild(e_TimerDing);
 }
