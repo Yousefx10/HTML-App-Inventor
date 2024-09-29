92ddf98f7fade02b9d4d6ba29919bd4f25d336d7
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-09-30 00:03:25 +0300

    CLICK EVENT IS WORKING DYNAMICALLY and is doing every task and every job without any issues

--- project/actions.js
+++ project/actions.js
@@ -1,20 +1,33 @@
 //All Running Actions Will Be Stated Here.
 function global_onClick(kitID){
 
+    //first of all is defining variables.
     let dynamicMap = window.parent.dynamicMap;
     let ACTIVEactions = window.parent.ACTIVEactions;
     let prefix = "click", contains = 'code'+kitID+".";
 
+    //then filtering the correct actions that are related to event
     ACTIVEactions
         .filter(blockID => blockID.startsWith(prefix) && blockID.includes(contains)) // Apply rules
         .forEach(blockID => {
 
-
-
-            const words = dynamicMap.get(blockID).split(',');
-            console.log(words);
+            const COMMANDS = dynamicMap.get(blockID).split(',');
+            doJOBS(COMMANDS);
 
         });
 
 
+}
+
+
+
+function doJOBS(COMMANDS){
+    switch (COMMANDS[0]) {
+        case "changetext":
+            document.getElementById(COMMANDS[1]).textContent=COMMANDS[2];
+            break;
+        case "changecolor":
+            document.getElementById(COMMANDS[1]).style.color=COMMANDS[2];
+            break;
+    }
 }
