81943035ee9d765df591c3f5841124cab104b60a
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-11-05 01:16:44 +0300

    Stating kit.js First

--- project.php
+++ project.php
@@ -162,9 +162,10 @@ function switchScreen()
 <script src="project/variables.js"></script>
 <script src="project/actions.js"></script>
 <script src="project/iframe.js"></script>
+<script src="project/kit.js"></script>
 <script src="project/project.js"></script>
 <script src="project/workspace.js"></script>
-<script src="project/kit.js"></script>
+
 <script src="project/interpreter.js"></script>
     </body>
 </html>
