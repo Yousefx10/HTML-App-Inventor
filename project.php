3ec13dd62b7419ea984fa751f8604d552e385c38
Yousef10 <yousef_espetan@yahoo.com>
2024-11-04 23:53:45 +0300

    Better ActButton

--- project.php
+++ project.php
@@ -96,7 +96,7 @@
     <div style="width:17.5em;">
         <!--will use it to add informations later, maybe screen controls?-->
         <!--i think maybe i will use it to add EXPORT button to save and download the project-->
-        <p style="font-size:10px">Version v.0.1.2</p>
+        <p style="font-size:10px">Version v.0.1.3</p>
     </div>
     <div class="iframe-container">
         <iframe frameborder="0" id="live_iframe" style="flex: 1;height:100%;width:100%;border:0;z-index: 1;"></iframe>
